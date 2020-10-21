baad2965d06a5d829b93e729fec07cacc2ec8f53
williamBMDK <williambillemeyling@gmail.com>
2020-10-21 17:15:41 +0200

    generic floyd_warshall

--- src/graph/floyd_warshall/main.cpp
+++ src/graph/floyd_warshall/main.cpp
@@ -1,31 +1,33 @@
-// INITIALIZE GRAPH WITH DIST 0 FROM NODE TO ITSELF
-vector<vector<int>> graph(n, vector<int>(n, INT_MAX));
-for (int i = 0; i < n; i++)
-    graph[i][i] = 0;
-
-// INPUT ALL EDGES AND UPDATE THE MIN WEIGHT
-for (int i = 0; i < m; i++) {
-    int u, v, w;
-    cin >> u >> v >> w;
-    graph[u][v] = min(graph[u][v], w);
+template<class Int>
+vector<vector<Int>> floyd_warshall(vector<vector<pair<int, Int>>>& adj, Int MX){
+	int n = (int)adj.size();
+	vector<vector<Int>> graph(n, vector<int>(n, MX));
+	// INITIALIZE GRAPH WITH DIST 0 FROM NODE TO ITSELF
+	for(int i = 0; i < n; i++) graph[i][i] = 0;
+	// INPUT ALL EDGES AND UPDATE THE MIN WEIGHT
+	for(int i = 0; i < n; i++){
+		for(auto& edge : adj[i]){
+			graph[i][edge.first] = min(graph[i][edge.first], edge.second);
+		}
+	}
+	// FLOYD WARSHALL
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (graph[i][k] != MX && graph[k][j] != MX)
+					graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j]);
+			}
+		}
+	}
+	// FLOYD WARSHALL APPROACH FOR CHECKING NEGATIVE WEIGHT CYCLE BETWEEN TWO NODES
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				if (graph[k][k] < 0 && graph[i][k] != MX && graph[k][j] != MX)
+					graph[i][j] = -MX;
+			}
+		}
+	}
+	return graph;
 }
 
-// FLOYD WARSHALL
-for (int k = 0; k < n; k++) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (graph[i][k] < INT_MAX && graph[k][j] < INT_MAX)
-                graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j]);
-        }
-    }
-}
-
-// FLOYD WARSHALL APPROACH FOR CHECKING NEGATIVE WEIGHT CYCLE BETWEEN TWO NODES
-for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-        for (int k = 0; k < n; k++) {
-            if (graph[k][k] < 0 && graph[i][k] != INT_MAX && graph[k][j] != INT_MAX)
-                graph[i][j] = -INT_MAX;
-        }
-    }
-}
--- src/graph/floyd_warshall/test1.cpp
+++ src/graph/floyd_warshall/test1.cpp
@@ -1,7 +1,41 @@
+// https://open.kattis.com/problems/allpairspath
 #include <bits/stdc++.h>
 using namespace std;
 #define print(x) cout << x << endl
 #define debug(x) cerr << #x << " : " << x << endl
+
+template<class Int>
+vector<vector<Int>> floyd_warshall(vector<vector<pair<int, Int>>>& adj, Int MX){
+	int n = (int)adj.size();
+	vector<vector<Int>> graph(n, vector<int>(n, MX));
+	// INITIALIZE GRAPH WITH DIST 0 FROM NODE TO ITSELF
+	for(int i = 0; i < n; i++) graph[i][i] = 0;
+	// INPUT ALL EDGES AND UPDATE THE MIN WEIGHT
+	for(int i = 0; i < n; i++){
+		for(auto& edge : adj[i]){
+			graph[i][edge.first] = min(graph[i][edge.first], edge.second);
+		}
+	}
+	// FLOYD WARSHALL
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (graph[i][k] != MX && graph[k][j] != MX)
+					graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j]);
+			}
+		}
+	}
+	// FLOYD WARSHALL APPROACH FOR CHECKING NEGATIVE WEIGHT CYCLE BETWEEN TWO NODES
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				if (graph[k][k] < 0 && graph[i][k] != MX && graph[k][j] != MX)
+					graph[i][j] = -MX;
+			}
+		}
+	}
+	return graph;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -17,37 +51,14 @@ int main() {
         if (!f) cout << endl;
         else f = false;
 
-        // INITIALIZE GRAPH WITH DIST 0 FROM NODE TO ITSELF
-        vector<vector<int>> graph(n, vector<int>(n, INT_MAX));
-        for (int i = 0; i < n; i++)
-            graph[i][i] = 0;
-
-        // INPUT ALL EDGES AND UPDATE THE MIN WEIGHT
+		vector<vector<pair<int,int>>> adj (n);
         for (int i = 0; i < m; i++) {
             int u, v, w;
             cin >> u >> v >> w;
-            graph[u][v] = min(graph[u][v], w);
-        }
-        
-        // FLOYD WARSHALL
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (graph[i][k] < INT_MAX && graph[k][j] < INT_MAX)
-						graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j]);
-                }
-            }
+			adj[u].push_back({v, w});
         }
 
-        // FLOYD WARSHALL APPROACH FOR CHECKING NEGATIVE WEIGHT CYCLE BETWEEN TWO NODES
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (graph[k][k] < 0 && graph[i][k] != INT_MAX && graph[k][j] != INT_MAX)
-                        graph[i][j] = -INT_MAX;
-                }
-            }
-        }
+		auto graph = floyd_warshall(adj, INT_MAX);
         
         for (int i = 0; i < q; i++) {
             int u, v;
