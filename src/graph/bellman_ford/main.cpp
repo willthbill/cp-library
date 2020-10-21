c50a9c2e462fa58001ee60cda9120230fb957941
williamBMDK <williambillemeyling@gmail.com>
2020-10-21 17:51:48 +0200

    making bellman ford more generic (support for long long) and retesting it

--- src/graph/bellman_ford/main.cpp
+++ src/graph/bellman_ford/main.cpp
@@ -1,25 +1,23 @@
-vector<int> bellman_ford(vector<vector<pair<int, int>>>& adj, int n, int m, int src) {
-
-    vector<int> dist(n, INT_MAX);
+template<class Int>
+vector<Int> bellman_ford(vector<vector<pair<int, Int>>>& adj, Int MX, int src) {
+	int n = (int)adj.size();
+    vector<Int> dist(n, MX);
     dist[src] = 0;
-    
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n; j++) {
-            for (int k = 0; k < adj[j].size(); k++) {
+            for (int k = 0; k < (int)adj[j].size(); k++) {
                 int u = j, v = adj[j][k].first, w = adj[j][k].second;
-                if (dist[u] != INT_MAX && dist[u]+w < dist[v])
+                if (dist[u] != MX && dist[u]+w < dist[v])
                     dist[v] = dist[u]+w;
             }
         }
     }
-
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < adj[i].size(); j++) {
+        for (int j = 0; j < (int)adj[i].size(); j++) {
             int u = i, v = adj[i][j].first, w = adj[i][j].second;
-            if (dist[u] != INT_MAX && dist[u]+w < dist[v])
+            if (dist[u] != MX && dist[u]+w < dist[v])
                 // NEGATIVE WEIGHT CYCLE DETECTED
         }
     }
-
     return dist;
 }
--- src/graph/bellman_ford/test1.cpp
+++ src/graph/bellman_ford/test1.cpp
@@ -1,36 +1,33 @@
+// https://open.kattis.com/problems/allpairspath
 #include <bits/stdc++.h>
 using namespace std;
 #define print(x) cout << x << endl
 #define debug(x) cerr << #x << " : " << x << endl
 
-vector<int> bellman_ford(vector<pair<int, int>>* adj, int n, int m, int src) {
-
-    vector<int> dist(n, INT_MAX);
+template<class Int>
+vector<Int> bellman_ford(vector<vector<pair<int, Int>>>& adj, Int MX, int src) {
+	int n = (int)adj.size();
+    vector<Int> dist(n, MX);
     dist[src] = 0;
-    
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n; j++) {
-            for (int k = 0; k < adj[j].size(); k++) {
+            for (int k = 0; k < (int)adj[j].size(); k++) {
                 int u = j, v = adj[j][k].first, w = adj[j][k].second;
-                if (dist[u] != INT_MAX && dist[u]+w < dist[v])
+                if (dist[u] != MX && dist[u]+w < dist[v])
                     dist[v] = dist[u]+w;
             }
         }
     }
-
-    // BELLMAN FORD ONLY CHECKS IF A NEGATIVE WEIGHT CYCLE EXISTS, NOT WHERE
-    /*
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < adj[i].size(); j++) {
+    /*for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (int)adj[i].size(); j++) {
             int u = i, v = adj[i][j].first, w = adj[i][j].second;
-            if (dist[u] != INT_MAX && dist[u]+w < dist[v])
+            if (dist[u] != MX && dist[u]+w < dist[v])
                 // NEGATIVE WEIGHT CYCLE DETECTED
         }
-    }
-    */
-
+    }*/
     return dist;
 }
+// only check for negative cycles, not where it is
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -46,7 +43,7 @@ int main() {
         if (!f) cout << endl;
         else f = false;
 
-        vector<pair<int, int>> adj[n];
+        vector<vector<pair<int, int>>> adj (n);
         vector<vector<int>> dist(n);
         
         for (int i = 0; i < m; i++) {
@@ -56,7 +53,7 @@ int main() {
         }
         
         for (int i = 0; i < n; i++)
-            dist[i] = bellman_ford(adj, n, m, i);
+            dist[i] = bellman_ford(adj, INT_MAX, i);
 
         // FLOYD WARSHALL APPROACH FOR CHECKING NEGATIVE WEIGHT CYCLE BETWEEN TWO NODES
         for (int i = 0; i < n; i++) {
