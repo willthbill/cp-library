c11d1e298a743adff0693805df2a1a47dd0ffc6c
Asthor Bjornsson <asthor02@gmail.com>
2020-10-25 23:13:40 +0100

    add prime factorize test and remove headers in prime exp table

--- src/number-theory/prime_exponentiation_table/main.cpp
+++ src/number-theory/prime_exponentiation_table/main.cpp
@@ -1,5 +1,5 @@
 vector<vector<ll>> prime_exponentiation_table(ll mx, vector<ll>&primes) { // stops at mx
-	vector<vi> mul(primes.size());
+	vector<vector<ll>> mul(primes.size());
 	for (int i = 0; i < primes.size() && primes[i] <= mx; i++) {
 		ll p = primes[i];
 		ll res = p;
