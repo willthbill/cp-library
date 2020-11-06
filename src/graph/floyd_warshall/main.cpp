7e09e8a68a6810c5fdf95ecb8b8fc7969f028f9b
williamBMDK <williambillemeyling@gmail.com>
2020-11-06 22:36:58 +0100

    update floyd-warshall

--- src/graph/floyd_warshall/main.cpp
+++ src/graph/floyd_warshall/main.cpp
@@ -7,15 +7,18 @@ vector<vector<Int>> floyd_warshall(vector<vector<pair<int, Int>>>& adj, Int MX){
 	// INPUT ALL EDGES AND UPDATE THE MIN WEIGHT
 	for(int i = 0; i < n; i++){
 		for(auto& edge : adj[i]){
-			graph[i][edge.first] = min(graph[i][edge.first], edge.second);
+			if(graph[i][edge.first] > edge.second){
+				graph[i][edge.first] = edge.second;
+			}
 		}
 	}
 	// FLOYD WARSHALL
 	for (int k = 0; k < n; k++) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (graph[i][k] != MX && graph[k][j] != MX)
-					graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j]);
+				if (graph[i][k] != MX && graph[k][j] != MX && graph[i][j] > graph[i][k] + graph[k][j]){
+					graph[i][j] = graph[i][k]+graph[k][j];
+				}
 			}
 		}
 	}
@@ -23,11 +26,11 @@ vector<vector<Int>> floyd_warshall(vector<vector<pair<int, Int>>>& adj, Int MX){
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			for (int k = 0; k < n; k++) {
-				if (graph[k][k] < 0 && graph[i][k] != MX && graph[k][j] != MX)
+				if (graph[k][k] < 0 && graph[i][k] != MX && graph[k][j] != MX){
 					graph[i][j] = -MX;
+				}
 			}
 		}
 	}
 	return graph;
 }
-
--- src/graph/floyd_warshall/test1.cpp
+++ src/graph/floyd_warshall/test1.cpp
@@ -7,21 +7,24 @@ using namespace std;
 template<class Int>
 vector<vector<Int>> floyd_warshall(vector<vector<pair<int, Int>>>& adj, Int MX){
 	int n = (int)adj.size();
-	vector<vector<Int>> graph(n, vector<int>(n, MX));
+	vector<vector<Int>> graph(n, vector<Int>(n, MX));
 	// INITIALIZE GRAPH WITH DIST 0 FROM NODE TO ITSELF
 	for(int i = 0; i < n; i++) graph[i][i] = 0;
 	// INPUT ALL EDGES AND UPDATE THE MIN WEIGHT
 	for(int i = 0; i < n; i++){
 		for(auto& edge : adj[i]){
-			graph[i][edge.first] = min(graph[i][edge.first], edge.second);
+			if(graph[i][edge.first] > edge.second){
+				graph[i][edge.first] = edge.second;
+			}
 		}
 	}
 	// FLOYD WARSHALL
 	for (int k = 0; k < n; k++) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (graph[i][k] != MX && graph[k][j] != MX)
-					graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j]);
+				if (graph[i][k] != MX && graph[k][j] != MX && graph[i][j] > graph[i][k] + graph[k][j]){
+					graph[i][j] = graph[i][k]+graph[k][j];
+				}
 			}
 		}
 	}
@@ -29,8 +32,9 @@ vector<vector<Int>> floyd_warshall(vector<vector<pair<int, Int>>>& adj, Int MX){
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			for (int k = 0; k < n; k++) {
-				if (graph[k][k] < 0 && graph[i][k] != MX && graph[k][j] != MX)
+				if (graph[k][k] < 0 && graph[i][k] != MX && graph[k][j] != MX){
 					graph[i][j] = -MX;
+				}
 			}
 		}
 	}
