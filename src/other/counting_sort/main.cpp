e2a543f16fe8117de220a022ba837141c019a2d9
Asthor Bjornsson <asthor02@gmail.com>
2020-11-29 01:31:56 +0100

    fix directory messup with counting sort

--- src/other/counting_sort/main.cpp
+++ src/other/counting_sort/main.cpp
@@ -1,10 +1,11 @@
+// sorts elements in range [0,max_val]
 template<class Int, class Auxiliary>
 vector<pair<Int, Auxiliary>> counting_sort(vector<pair<Int, Auxiliary>> A, int max_val) {
 	int n = A.size();
-	vector<int> cnt(n + 1, 0);
+	vector<int> cnt(max_val + 1, 0);
 	for (int i = 0; i < n; i++)
 		cnt[A[i].fs + 1]++;
-	for (int i = 1; i < n; i++)
+	for (int i = 1; i <= max_val; i++)
 		cnt[i] += cnt[i - 1];
 	vector<pair<Int, Auxiliary>> res(n);
 	for (int i = 0; i < n; i++)
