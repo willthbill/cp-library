f7eb0240578e70b3b5d9559707ac3e7ec67c3362
Asthor Bjornsson <asthor02@gmail.com>
2020-12-29 23:15:37 +0000

    dependancy bug fix

--- src/number-theory/binomial_coefficient/main.cpp
+++ src/number-theory/binomial_coefficient/main.cpp
@@ -1,6 +1,6 @@
 %include<number-theory/mod_pow>%
 
-ll nck(ll n, ll k, ll m, vector<ll>& factorials) { // m is big prime
+ll choose(ll n, ll k, ll m, vector<ll>& factorials) { // m is big prime
 	assert(n >= k);
 	assert(int(factorials.size()) > n);
 	return factorials[n] * mod_pow(factorials[n - k] * factorials[k] % m, m - 2, m) % m;
