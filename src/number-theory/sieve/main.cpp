f13e95e9dc37f1b43f425ca3b86e7f9a8848c2ae
asthor24 <asthor02@gmail.com>
2021-10-08 17:55:19 +0200

    optimizes sieve

--- src/number-theory/sieve/main.cpp
+++ src/number-theory/sieve/main.cpp
@@ -1,11 +1,16 @@
+// change MX to change for which n you want to calculate all primes p such that p <= n
+// this version is optimized for memory, using bitsets, which are great
+const int MX = 1e8;
+bitset<MX> + 1> cnt;
 vector<int> sieve(int n) {
-	vector<int> cnt(n + 1, 0);
-	for(int i = 2; i <= n; i++)
-		for(int j = i; j <= n; j += i)
-			cnt[j]++;
-	vector<int> ans;
-	for(int i = 2; i <= n; i++)
-		if(cnt[i] == 1)
-			ans.pb(i);
-	return ans;
+    for (int i = 2; i <= n; i++) {
+        if (cnt[i])
+            continue;
+        for (int j = i*i; j <= n; j += i)
+            cnt[j] = 1;
+    }
+    vector<int> primes;
+    for (int i = 2; i <= n; i++)
+        if(!cnt[i]) primes.pb(i);
+    return primes;
 }
