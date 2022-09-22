c62b6396fcb4e47e547dd9867576e050c5063a62
chri80d2 <44302442+chri80d2@users.noreply.github.com>
2022-09-23 00:33:08 +0200

    23-09-2022

---
+++ src/christian-txt-files/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define printp(x, y) for (auto z = x.begin(); z != x.end(); z++) cout << (*z).first << " " << (*z).second << (z == x.end()-1 ? "\n" : y)
+#define printl(x, y) for (auto z = x.begin(); z != x.end(); z++) cout << *z << (z == x.end()-1 ? "\n" : y)
+#define readp(x, y) for (int z = 0; z < y; z++) cin >> x[z].first >> x[z].second
+#define nextpow2(x) __builtin_popcount(x) == 1 ? x : (1 << (32-__builtin_clz(x)))
+#define licorice ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define read(x, y) for (int z = 0; z < y; z++) cin >> x[z]
+#define mkunique(x) x.resize(unique(all(x))-x.begin())
+#define debug(x) cerr << #x << " : " << x << endl
+#define print(x) cout << x << endl
+#define all(x) x.begin(), x.end()
+#define len(x) (int)x.size()
+#define pb push_back
+#define i64 int64_t
+#define u64 uint64_t
+#define d64 double_t
+#define oo INT64_MAX
+#define fi first
+#define se second
+
+void solve() {
+    //
+}
+
+int main() {
+    
+    /* 甘草 */
+    licorice
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
